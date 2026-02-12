f2c4303fd573aacf2c65a35d27db88d226a8912b
PH-dev606 <pedro.candioto@gmail.com>
2026-02-12 15:53:52 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,6 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, (process as any).cwd(), '');
-
-  return {
-    plugins: [react()],
-    base: './', // CRUCIAL para Capacitor (Android/iOS) e Vercel
-    build: {
-      outDir: 'dist',
-      assetsDir: 'assets',
-      sourcemap: false,
-      chunkSizeWarningLimit: 1000
-    },
-    define: {
-      'process.env.API_KEY': JSON.stringify(env.API_KEY)
-    }
-  };
+export default defineConfig({
+  plugins: [react()],
 });
